ada6fb918148a8f9d9ce5e99085379f0c58da1f2
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-09-30 15:43:25 +0700

    add feed forward

--- encoder/convolution.py
+++ encoder/convolution.py
@@ -58,24 +58,3 @@ class ConformerConvolution(torch.nn.Module):
         x = self.pointwise_conv2(x)
         x = x.transpose(1, 2)
         return x
-
-
-class ConformerFeedForward(torch.nn.Module):
-    """
-    feed-forward module of Conformer model.
-    """
-
-    def __init__(self, d_model, d_ff, dropout, activation=torch.nn.SiLU()):
-        super(ConformerFeedForward, self).__init__()
-        self.linear1 = torch.nn.Linear(d_model, d_ff)
-        self.activation = activation
-        self.dropout = torch.nn.Dropout(p=dropout)
-        self.linear2 = torch.nn.Linear(d_ff, d_model)
-
-    def forward(self, x):
-        x = self.linear1(x)
-        x = self.activation(x)
-        x = self.dropout(x)
-        x = self.linear2(x)
-        return x
-
--- encoder/feed_forward.py
+++ encoder/feed_forward.py
@@ -1,1 +1,22 @@
+import torch
+
+
+class ConformerFeedForward(torch.nn.Module):
+    """
+    feed-forward module of Conformer model.
+    """
+
+    def __init__(self, d_model, d_ff, dropout, activation=torch.nn.SiLU()):
+        super(ConformerFeedForward, self).__init__()
+        self.linear1 = torch.nn.Linear(d_model, d_ff)
+        self.activation = activation
+        self.dropout = torch.nn.Dropout(p=dropout)
+        self.linear2 = torch.nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
 

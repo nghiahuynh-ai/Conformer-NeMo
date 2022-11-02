0e6ee6dc4fd12033e39e514389cf6e384ab37251
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-02 13:43:23 +0700

    delete scripts

--- scripts/asr_language_modeling/ngram_lm/install_beamsearch_decoders.sh
+++
@@ -1,8 +0,0 @@
-# install Boost package
-sudo apt-get install build-essential libboost-all-dev cmake zlib1g-dev libbz2-dev liblzma-dev
-git clone https://github.com/NVIDIA/OpenSeq2Seq -b ctc-decoders
-mv OpenSeq2Seq/decoders .
-rm -rf OpenSeq2Seq
-cd decoders
-./setup.sh
-cd ..

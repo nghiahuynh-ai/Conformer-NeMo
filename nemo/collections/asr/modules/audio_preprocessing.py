6dae24e23de6bc4c8079ffa76154771bf64d02ed
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-01-06 15:47:32 +0700

    update white noise & mhsa

--- nemo/collections/asr/modules/audio_preprocessing.py
+++ nemo/collections/asr/modules/audio_preprocessing.py
@@ -285,8 +285,8 @@ class AudioToMelSpectrogramPreprocessor(AudioPreprocessor):
         if self.mix_white_noise:
             variance = np.random.uniform(self.min_white_noise_var, self.max_white_noise_var)
             noise = np.random.normal(self.white_noise_mean, variance, size=input_signal.shape)
-            noise = torch.from_numpy(noise).to(input_signal.device)
-            input_signal = input_signal + noise.type(torch.FloatTensor)
+            noise = torch.from_numpy(noise).type(torch.FloatTensor)
+            input_signal = input_signal + noise.to(input_signal.device)
         return self.featurizer(input_signal, length)
     
     @property

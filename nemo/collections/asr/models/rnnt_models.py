47bb14f03adfaaf37e1be48748c13aa4b463193e
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-02-01 10:48:28 +0700

    add pseudo labeling

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -688,6 +688,7 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             decoder, target_length, states = self.decoder(targets=transcript, target_length=transcript_len)
         else:
             with torch.no_grad():
+                print(batch_nb)
                 decoder, target_length, states = self.decoder(targets=transcript, target_length=transcript_len)
         
         if hasattr(self, '_trainer') and self._trainer is not None:

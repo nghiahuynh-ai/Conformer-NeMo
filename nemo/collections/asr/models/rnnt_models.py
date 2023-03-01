176c2db9b337933debf45da57f667f60f62252b8
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-01 17:39:15 +0700

    update

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -683,8 +683,7 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             )
         
         if self.speech_enhance is not None:
-            with torch.autograd.set_detect_anomaly(True):
-                processed_signal = self.speech_enhance(processed_signal)
+            processed_signal = self.speech_enhance(processed_signal)
         
         # Spec augment is not applied during evaluation/testing
         if (self.spec_augmentation is not None) and self.training:

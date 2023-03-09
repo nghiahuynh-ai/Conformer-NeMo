09a53265905fa0cec5632caf73a86579215b576a
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-09 23:18:13 +0700

    update

--- nemo/collections/asr/data/audio_to_text.py
+++ nemo/collections/asr/data/audio_to_text.py
@@ -66,8 +66,8 @@ def _speech_collate_fn(batch, pad_id, hop_len, downsize_factor):
     if has_audio:
         max_audio_len = max(audio_lengths).item()
         if hop_len is not None and downsize_factor is not None:
-            n_feats = int(math.ceil(max_audio_len / hop_len))
-            max_feats = int(math.ceil(n_feats / downsize_factor) * downsize_factor)
+            n_feats = int(max_audio_len / hop_len + 1)
+            max_feats = int(n_feats / downsize_factor + 1) * downsize_factor
             max_audio_len = max_feats * hop_len
             
     max_tokens_len = max(tokens_lengths).item()

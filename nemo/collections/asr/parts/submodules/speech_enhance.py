5e0c5660f6bfb010eec2c120dd780d2a74380173
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-11 09:19:03 +0700

    update

--- nemo/collections/asr/parts/submodules/speech_enhance.py
+++ nemo/collections/asr/parts/submodules/speech_enhance.py
@@ -129,18 +129,18 @@ class SEConvModule(nn.Module):
         super().__init__()
         
         self.conv_in = nn.Conv2d(
-            in_channels=1,
-            out_channels=conv_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
-            )
-        self.norm_in = nn.BatchNorm2d(conv_channels)
-        self.conv_out = nn.Conv2d(
             in_channels=conv_channels,
             out_channels=conv_channels,
             kernel_size=3,
             stride=2,
+            padding=1,
+            )
+        self.norm_in = nn.BatchNorm2d(conv_channels)
+        self.conv_out = nn.Conv2d(
+            in_channels=1,
+            out_channels=conv_channels,
+            kernel_size=3,
+            stride=1,
             padding=1,
             )
         self.norm_out = nn.BatchNorm2d(conv_channels)
@@ -166,21 +166,21 @@ class SEConvTransposedModule(nn.Module):
     def __init__(self, dim_in, dim_out, conv_channels):
         super().__init__()
 
-        self.conv_in = nn.ConvTranspose2d(
+        self.conv_in = nn.Conv2d(
+            in_channels=conv_channels,
+            out_channels=conv_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            )
+        self.norm_in = nn.BatchNorm2d(conv_channels)
+        self.conv_out = nn.ConvTranspose2d(
             in_channels=1,
             out_channels=conv_channels,
             kernel_size=3,
             stride=2,
             padding=1,
             output_padding=1,
-            )
-        self.norm_in = nn.BatchNorm2d(conv_channels)
-        self.conv_out = nn.Conv2d(
-            in_channels=conv_channels,
-            out_channels=conv_channels,
-            kernel_size=3,
-            stride=1,
-            padding=1,
             )
         self.norm_out = nn.BatchNorm2d(conv_channels)
         self.proj_out = nn.Linear(conv_channels * (2 * dim_in), dim_out)

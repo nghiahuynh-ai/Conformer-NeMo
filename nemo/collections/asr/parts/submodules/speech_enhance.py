e12d4043969e24ddf5c6bf3a426cf2be83f60e6f
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-13 14:29:27 +0700

    update

--- nemo/collections/asr/parts/submodules/speech_enhance.py
+++ nemo/collections/asr/parts/submodules/speech_enhance.py
@@ -37,7 +37,8 @@ class SpeechEnhance(nn.Module):
         )
         
     def forward_encoder(self, x):
-        return self.encoder(x), torch.tensor([int(x / self.scaling_factor)], dtype=torch.int, device=x.device)
+        length = int(length / self.scaling_factor)
+        return self.encoder(x), torch.tensor(length, dtype=torch.int, device=x.device)
     
     def forward_decoder(self, x):
         return self.decoder(x, self.encoder.layers_out)

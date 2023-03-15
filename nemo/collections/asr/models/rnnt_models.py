ad0418d43053ae5f0093f344d6eadd53cf5d2a3c
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-15 09:39:09 +0700

    update

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -96,7 +96,7 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
                 scaling_factor=self._cfg.speech_enhance.scaling_factor,
                 n_features=self._cfg.speech_enhance.n_feats,
                 asr_d_model=self._cfg.encoder.d_model,
-                conv_channels=self._cfg.encoder.conv_channels,
+                conv_channels=self._cfg.speech_enhance.conv_channels,
             )
 
         else:

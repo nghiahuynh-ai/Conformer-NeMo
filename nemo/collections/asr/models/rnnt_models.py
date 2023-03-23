6197898e8db92ec9f624bc9c1db1494ce1d351c8
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-03-23 14:38:14 +0700

    update

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -783,7 +783,7 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             self._optim_normalize_txu = [encoded_len.max(), transcript_len.max()]
             
         if self.speech_enhance is not None:
-            loss_value = 1e-5 * loss_value + loss_se
+            loss_value = loss_value + loss_se
 
         return {'loss': loss_value}
 

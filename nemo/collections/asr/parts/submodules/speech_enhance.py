874b540eac7045285281558597cf097847fa5476
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-04-01 15:26:19 +0700

    update

--- nemo/collections/asr/parts/submodules/speech_enhance.py
+++ nemo/collections/asr/parts/submodules/speech_enhance.py
@@ -75,10 +75,10 @@ class SEEncoder(nn.Module):
             
     def forward(self, x):
         # x: (b, t, d)
-        
-        self.enc_out.clear()
+    
         x = x.unsqueeze(1)
         
+        self.enc_out = [x]
         for layer in self.layers:
             x = nn.functional.relu(layer(x))
             self.enc_out = [x] + self.enc_out
@@ -115,6 +115,7 @@ class SEDecoder(nn.Module):
         for ith, layer in enumerate(self.layers):
             x = x + enc_out[ith]
             x = layer(x)
+        x = x + enc_out[-1]
 
         x = x.squeeze(1)
         x = x.transpose(1, 2)
@@ -242,51 +243,51 @@ def calc_length(lengths, padding, kernel_size, stride, ceil_mode, repeat_num=1):
     return lengths.to(dtype=torch.int)
 
 
-# class PositionalEncoding2D(nn.Module):
-#     def __init__(self, channels):
-#         """
-#         :param channels: The last dimension of the tensor you want to apply pos emb to.
-#         """
-#         super(PositionalEncoding2D, self).__init__()
-#         self.org_channels = channels
-#         channels = int(np.ceil(channels / 4) * 2)
-#         self.channels = channels
-#         inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
-#         self.register_buffer("inv_freq", inv_freq)
-#         self.cached_penc = None
+class PositionalEncoding2D(nn.Module):
+    def __init__(self, channels):
+        """
+        :param channels: The last dimension of the tensor you want to apply pos emb to.
+        """
+        super(PositionalEncoding2D, self).__init__()
+        self.org_channels = channels
+        channels = int(np.ceil(channels / 4) * 2)
+        self.channels = channels
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, channels, 2).float() / channels))
+        self.register_buffer("inv_freq", inv_freq)
+        self.cached_penc = None
 
-#     def forward(self, tensor):
-#         """
-#         :param tensor: A 4d tensor of size (batch_size, x, y, ch)
-#         :return: Positional Encoding Matrix of size (batch_size, x, y, ch)
-#         """
-#         if len(tensor.shape) != 4:
-#             raise RuntimeError("The input tensor has to be 4d!")
+    def forward(self, tensor):
+        """
+        :param tensor: A 4d tensor of size (batch_size, x, y, ch)
+        :return: Positional Encoding Matrix of size (batch_size, x, y, ch)
+        """
+        if len(tensor.shape) != 4:
+            raise RuntimeError("The input tensor has to be 4d!")
 
-#         if self.cached_penc is not None and self.cached_penc.shape == tensor.shape:
-#             return self.cached_penc
+        if self.cached_penc is not None and self.cached_penc.shape == tensor.shape:
+            return self.cached_penc
 
-#         self.cached_penc = None
-#         batch_size, x, y, orig_ch = tensor.shape
-#         pos_x = torch.arange(x, device=tensor.device).type(self.inv_freq.type())
-#         pos_y = torch.arange(y, device=tensor.device).type(self.inv_freq.type())
-#         sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
-#         sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
-#         emb_x = get_emb(sin_inp_x).unsqueeze(1)
-#         emb_y = get_emb(sin_inp_y)
-#         emb = torch.zeros((x, y, self.channels * 2), device=tensor.device).type(
-#             tensor.type()
-#         )
-#         emb[:, :, : self.channels] = emb_x
-#         emb[:, :, self.channels : 2 * self.channels] = emb_y
+        self.cached_penc = None
+        batch_size, x, y, orig_ch = tensor.shape
+        pos_x = torch.arange(x, device=tensor.device).type(self.inv_freq.type())
+        pos_y = torch.arange(y, device=tensor.device).type(self.inv_freq.type())
+        sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
+        sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
+        emb_x = get_emb(sin_inp_x).unsqueeze(1)
+        emb_y = get_emb(sin_inp_y)
+        emb = torch.zeros((x, y, self.channels * 2), device=tensor.device).type(
+            tensor.type()
+        )
+        emb[:, :, : self.channels] = emb_x
+        emb[:, :, self.channels : 2 * self.channels] = emb_y
 
-#         self.cached_penc = emb[None, :, :, :orig_ch].repeat(tensor.shape[0], 1, 1, 1)
-#         return self.cached_penc
+        self.cached_penc = emb[None, :, :, :orig_ch].repeat(tensor.shape[0], 1, 1, 1)
+        return self.cached_penc
 
 
-# def get_emb(sin_inp):
-#     """
-#     Gets a base embedding for one dimension with sin and cos intertwined
-#     """
-#     emb = torch.stack((sin_inp.sin(), sin_inp.cos()), dim=-1)
-#     return torch.flatten(emb, -2, -1)
+def get_emb(sin_inp):
+    """
+    Gets a base embedding for one dimension with sin and cos intertwined
+    """
+    emb = torch.stack((sin_inp.sin(), sin_inp.cos()), dim=-1)
+    return torch.flatten(emb, -2, -1)

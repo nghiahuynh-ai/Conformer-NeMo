49f873ec36ab766fa309c3b9d9ee5ec1e1397762
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2023-04-04 18:53:48 +0700

    update

--- nemo/collections/asr/parts/preprocessing/features.py
+++ nemo/collections/asr/parts/preprocessing/features.py
@@ -65,7 +65,7 @@ def normalize_batch(x, seq_len, normalize_type):
             x_std[i, :] = x[i, :, : seq_len[i]].std(dim=1)
         # make sure x_std is not zero
         x_std += CONSTANT
-        return (x - x_mean.unsqueeze(2)) / x_std.unsqueeze(2)
+        return (x - x_mean.unsqueeze(2)) / x_std.unsqueeze(2), x_mean, x_std
     elif normalize_type == "all_features":
         x_mean = torch.zeros(seq_len.shape, dtype=x.dtype, device=x.device)
         x_std = torch.zeros(seq_len.shape, dtype=x.dtype, device=x.device)
@@ -74,13 +74,13 @@ def normalize_batch(x, seq_len, normalize_type):
             x_std[i] = x[i, :, : seq_len[i].item()].std()
         # make sure x_std is not zero
         x_std += CONSTANT
-        return (x - x_mean.view(-1, 1, 1)) / x_std.view(-1, 1, 1)
+        return (x - x_mean.view(-1, 1, 1)) / x_std.view(-1, 1, 1), x_mean, x_std
     elif "fixed_mean" in normalize_type and "fixed_std" in normalize_type:
         x_mean = torch.tensor(normalize_type["fixed_mean"], device=x.device)
         x_std = torch.tensor(normalize_type["fixed_std"], device=x.device)
-        return (x - x_mean.view(x.shape[0], x.shape[1]).unsqueeze(2)) / x_std.view(x.shape[0], x.shape[1]).unsqueeze(2)
+        return (x - x_mean.view(x.shape[0], x.shape[1]).unsqueeze(2)) / x_std.view(x.shape[0], x.shape[1]).unsqueeze(2), x_mean, x_std
     else:
-        return x
+        return x, 0.0, 0.0
 
 
 def splice_frames(x, frame_splicing):
@@ -434,7 +434,8 @@ class FilterbankFeatures(nn.Module):
 
         # normalize if required
         if self.normalize:
-            x = normalize_batch(x, seq_len, normalize_type=self.normalize)
+            x, mean, std = normalize_batch(x, seq_len, normalize_type=self.normalize)
+            self.norm = (mean, std)
 
         # mask to zero any values beyond seq_len in batch, pad to multiple of `pad_to` (for efficiency)
         max_len = x.size(-1)
@@ -451,3 +452,6 @@ class FilterbankFeatures(nn.Module):
                 x = nn.functional.pad(x, (0, pad_to - pad_amt), value=self.pad_value)
                 
         return x, seq_len
+    
+    def inverse(self, x):
+        pass
